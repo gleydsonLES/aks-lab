1c6878f83790cd71b75e1888ca054c4fa130894f
Gleydson Epifanio <gles@outlook.com.br>
2024-05-19 12:25:38 -0300

    Setup plan aks

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aks-cert-letencrypt"
+  location = "eastus2"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cert-lets"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-cert"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "lab"
+  }
+}
