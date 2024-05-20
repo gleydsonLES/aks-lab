a7e8ec97caecdf23cfe6903444178b08f039b2be
Gleydson Epifanio <gles@outlook.com.br>
2024-05-19 22:05:48 -0300

    Setup plan aks

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "teste"
+  name     = "AKS"
   location = "eastus2"
 }
 
--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,5 @@ output "resource_group_name" {
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
+
+
