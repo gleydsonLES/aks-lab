e7ada8de440b0fddb03317770eca92d9bf0d2346
Gleydson Epifanio <gles@outlook.com.br>
2024-05-20 10:51:54 -0300

    Setup aks

--- aks/backend.tf
+++ aks/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "storagegleydson"
     storage_account_name = "tfstategleydson"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "terraform-aks.tfstate"
   }
 }
